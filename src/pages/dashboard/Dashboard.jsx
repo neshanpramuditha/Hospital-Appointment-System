2009e250352f7b61edf5cc417310e082202214fa
neshanpramuditha <neshanpppramuditha@gmail.com>
2026-06-26 18:37:09 +0530

    Vite import analysis error

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Sidebar from '../components/Sidebar';
-import Navbar from '../components/Navbar';
+import Sidebar from '../../components/Sidebar';
+import Navbar from '../../components/Navbar';
 
 function Dashboard() {
   const totalDoctors = 12;
