3469d1e200d9045b90483b59257d4d8d328967ea
neshanpramuditha <neshanpppramuditha@gmail.com>
2026-06-26 15:48:12 +0530

    Set login route

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,11 +4,13 @@ import Dashboard from '../pages/Dashboard'
 import Doctors from '../pages/Doctors'
 import Patients from '../pages/Patients'
 import Appointments from '../pages/Appointments'
+import Login from '../pages/auth/Login'
 
 function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
         <Route path="/" element={<Dashboard />} />
         <Route path="/doctors/*" element={<Doctors />} />
         <Route path="/patients/*" element={<Patients />} />
