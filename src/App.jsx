b3a2aa8fdf6c9fd943114c3328d3a774e3980f9b
neshanpramuditha <neshanpppramuditha@gmail.com>
2026-06-26 14:43:45 +0530

    Implement Basic Structure of login page and add it in to the APP.jsx as a component, Declare colors for web

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
+import Clinexa from './pages/auth/Login'
 import DoctorsList from './pages/doctors/DoctorsList'
+
 
 function App() {
     return (
-    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
-      <div className="w-96 h-96 bg-white rounded-lg shadow-lg flex items-center justify-center"> <DoctorsList /> </div>
-      <h1 className="text-5xl font-bold text-blue-600">
-        Tailwind CSS is Working 🎉
-      </h1>
+    <div>
+      <Clinexa/>
     </div>
   )
     
---
+++ src/pages/dashboard/Dashboard.jsx
@@ -0,0 +1,16 @@
+function Dashboard() {
+  return (
+          <div>
+            <div>
+              <h1>
+                Dashboard Page
+              </h1>
+            </div>
+
+            <h1 className="text-5xl font-bold text-blue-600">
+              Tailwind CSS is Working 🎉
+            </h1>
+          </div>
+  );
+}
+export default Dashboard;
